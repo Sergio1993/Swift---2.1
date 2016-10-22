3e15543e7e8a16801670f7ddb3c5050f270543cd
Sergio Pita <pita310593@gmail.com>
2016-10-22 10:33:26 +0200

    no message

--- ProyectoFacturacion/ProyectoFacturacion/Clientes.swift
+++ ProyectoFacturacion/ProyectoFacturacion/Clientes.swift
@@ -15,25 +15,36 @@ class Clientes: UIViewController, UITableViewDataSource {
     
     var nombresArray:[String] = [String]()
     var apeArray:[String] = [String]()
+    var clientesArray = [GuardarClientes]()
     
     var nombre: String = ""
     var apellido: String = ""
     
     
     @IBAction func done(_ segue:UIStoryboardSegue) {
-        nombresArray.append(nombre)
-        apeArray.append(apellido)
+        //nombresArray.append(nombre)
+        //apeArray.append(apellido)
+        saveCustomers(nom: nombre, ape: apellido)
+        print("DONE NOMBRE: "+nombre)
+        print("DONE APELLIDO: "+apellido)
     }
     
     override func viewDidLoad() {
-        tableClientes.dataSource = self
         super.viewDidLoad()
+        print("viewDidLoad NOMBRE: "+nombre)
+        print("viewDidLoad APELLIDO: "+apellido)
+        saveCustomers(nom: nombre, ape: apellido)
+        tableClientes.dataSource = self
+        
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         tableClientes.dataSource = self
+        print("viewWillAppear NOMBRE: "+nombre)
+        print("viewWillAppear APELLIDO: "+apellido)
         tableClientes.reloadData()
+        //saveCustomers()
     }
     
     
@@ -42,17 +53,41 @@ class Clientes: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nombresArray.count
+        return clientesArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+       
         let celda = tableView.dequeueReusableCell(withIdentifier: "CeldaReusable", for: indexPath)
         
-        celda.textLabel!.text = nombresArray[(indexPath as NSIndexPath).row]
-        celda.detailTextLabel!.text = apeArray[(indexPath as NSIndexPath).row]
+        if let data = UserDefaults.standard.object(forKey: "clientes") as? Data {
+            if let clientes = NSKeyedUnarchiver.unarchiveObject(with: data) as? [GuardarClientes]
+            {
+                print("tableView NOMBRE: "+nombre)
+                print("tableView APELLIDO: "+apellido)
+                celda.textLabel!.text = "\( clientes[(indexPath as NSIndexPath).row])"
+                celda.detailTextLabel!.text = "\( clientes[(indexPath as NSIndexPath).row])"
+                //celda.textLabel!.text = nombresArray[(indexPath as NSIndexPath).row]
+                //celda.detailTextLabel!.text = apeArray[(indexPath as NSIndexPath).row]
+            }
+        }
         
         return celda
+    }
+    
+    func saveCustomers(nom: String, ape: String){
+        
+        print("saveCustomers NOMBRE: "+nombre)
+        print("saveCustomers APELLIDO: "+apellido)
+        
+        let item1 = GuardarClientes(nombre: nom, apellido: ape)
+        
+        
+        clientesArray.append(item1)
+        
+        let placesData = NSKeyedArchiver.archivedData(withRootObject: clientesArray)
+        UserDefaults.standard.set(placesData, forKey: "clientes")
+        
     }
 
     
---
+++ ProyectoFacturacion/ProyectoFacturacion/GuardarClientes.swift
@@ -0,0 +1,31 @@
+//
+//  GuardarClientes.swift
+//  ProyectoFacturacion
+//
+//  Created by Sergio Pita on 22/10/16.
+//  Copyright © 2016 Sergio Pita. All rights reserved.
+//
+
+import Foundation
+
+class GuardarClientes : NSObject {
+    
+    var nombre : String
+    var apellido : String
+    
+    
+    init(nombre: String, apellido: String){
+        self.nombre = nombre
+        self.apellido = apellido
+    }
+    
+    func encodeWithCoder(_ aCoder: NSCoder) {
+        aCoder.encode(nombre, forKey: "nombre")
+        aCoder.encode(apellido, forKey: "apellido")
+    }
+    
+    required init(coder aDecoder:NSCoder) {
+        nombre = aDecoder.decodeObject(forKey: "nombre") as! String
+        apellido = aDecoder.decodeObject(forKey: "apellido") as! String
+    }
+}
